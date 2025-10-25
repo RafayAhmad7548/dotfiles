9ecd9a7cc2d6ef0546235ce5b40d23fade4f8977
RafayAhmad7548 <rafayahmad1425@gmail.com>
2025-10-25 21:04:55 +0500

    normal update

--- scripts/brightnessWidget.sh
+++ scripts/brightnessWidget.sh
@@ -3,20 +3,20 @@
 while true; do
 	while "$(cat /home/rafayahmad/dotfiles/.config/eww/brightnessShow)"; do
 
-		activeWindows=$(/home/rafayahmad/eww/target/release/eww active-windows)
+		activeWindows=$(eww active-windows)
 		if [[ "$activeWindows" != *"brightnessWin"* ]] ; then
 			echo "opening"
-			/home/rafayahmad/eww/target/release/eww open brightnessWin
+			eww open brightnessWin
 		fi
 
 		echo false > /home/rafayahmad/.config/eww/brightnessShow
 		sleep 1
 	done
 	
-	activeWindows=$(/home/rafayahmad/eww/target/release/eww active-windows)
+	activeWindows=$(eww active-windows)
 	if [[ "$activeWindows" == *"brightnessWin"* ]] ; then
 		echo "closing"
-		/home/rafayahmad/eww/target/release/eww close brightnessWin
+		eww close brightnessWin
 	fi
 
 done
